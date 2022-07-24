cf32b2da5f70912907358497ffdbf82a5b585c86
Pavel Popov <legeon48@mail.ru>
2022-07-24 12:12:18 +0300

    Добавлена страница ресторан

---
+++ WebHomeDay/Pages/Restaurants/restaurant.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebHomeDay.Pages.Restaurants
+{
+    public class restaurantModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
